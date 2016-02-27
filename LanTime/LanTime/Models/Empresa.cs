fb4d284961cab4e6f4b4cd0889af7771a204c827
RoenMidnight <brunocesara@hotmail.com>
2016-02-27 12:25:30 -0300

    Adding some New Information

---
+++ LanTime/LanTime/Models/Empresa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lantime.Models
+{
+    public class Empresa
+    {
+        [Required]
+        [DisplayName("Empresa ID")]
+        public int EmpresaId { get; set; }
+
+        [Required]
+        [DisplayName("Código")]
+        public int Codig_Empr { get; set; }
+
+        [Required]
+        [DisplayName("Razao")]
+        public string Razao_Empr { get; set; }
+
+        [Required]
+        [DisplayName("CNPJ")]
+        public string ECNPJ_Empr { get; set; }
+
+    }
+}
---
+++ LanTime/LanTime/Models/LocalDeTrabalho.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lantime.Models
+{
+    public class LocalDeTrabalho
+    {
+        [Required]
+        [DisplayName("Local de Trabalho ID")]
+        public int LocalDeTrabalhoId { get; set; }
+
+        [Required]
+        [DisplayName("Código")]
+        public int Codig_Loca { get; set; }
+
+        [DisplayName("Descrição")]
+        public string Descr_Loca { get; set; }
+
+        [Required]
+        [DisplayName("Empresa")]
+        public int EmpresaId { get; set; }
+        public virtual Empresa Empresa { get; set; }
+    }
+}
